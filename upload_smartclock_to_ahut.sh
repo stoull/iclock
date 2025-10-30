bafc2a6b16ca98714c03d572c3c1bb1d259c7a8a
hut <changchun.chen@growatt.com>
2025-10-30 12:40:06 +0800

    update scripts

--- upload_smartclock_to_ahut.sh
+++ upload_smartclock_to_ahut.sh
@@ -5,4 +5,5 @@ npm run build
 zip -r ./smartclock_build.zip ./build
 zip -d ./smartclock_build.zip "__MACOSX*"
 scp ./smartclock_build.zip hut@ahut.site:~/
-ssh hut@ahut.site "cd ~; SUDO_PASSWORD=$PASSWORD ./update_smartclock.sh;"
+ssh hut@ahut.site "cd ~; SUDO_PASSWORD=$PASSWORD ./scripts/update_smartclock.sh;"
+unset PASSWORD
--- upload_smartclock_to_pi.sh
+++ upload_smartclock_to_pi.sh
@@ -5,4 +5,5 @@ npm run build
 zip -r ./smartclock_build.zip ./build
 zip -d ./smartclock_build.zip "__MACOSX*"
 scp ./smartclock_build.zip pi@hutpi.local:~/
-ssh pi@hutpi.local "cd ~; SUDO_PASSWORD=$PASSWORD ./update_smartclock.sh;"
+ssh pi@hutpi.local "cd ~; SUDO_PASSWORD=$PASSWORD ./scripts/update_smartclock.sh;"
+unset PASSWORD
