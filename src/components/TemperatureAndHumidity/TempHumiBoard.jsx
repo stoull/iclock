4e787f7c5439afbd09508431693d6a765cdfa9a3
hut <changchun.chen@growatt.com>
2025-12-18 12:46:07 +0800

    change api

--- src/components/TemperatureAndHumidity/TempHumiBoard.jsx
+++ src/components/TemperatureAndHumidity/TempHumiBoard.jsx
@@ -77,9 +77,9 @@ class TempHumiBoard extends React.Component {
             // );
         
             const tData = await smartClockService.getCurrentTempInfo();
-            this.handleTempInfoChange(tData);
+            this.handleTempInfoChange(tData.data);
             const hData = await smartClockService.getTempInfoHistory(); 
-            this.setState({ chartData: hData, loading: false, error: null });
+            this.setState({ chartData: hData.data, loading: false, error: null });
         } catch (error) {
             this.setState({ loading: false, error: error.message });
         }
